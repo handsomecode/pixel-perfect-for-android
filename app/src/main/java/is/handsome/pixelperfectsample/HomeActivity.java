f79ef821c494734de89ec7de2aafe548330b43bf
Olga Konoreva <olgakonoreva@gmail.com>
2015-12-01 19:53:11 +0600

    Add second activity, update layouts for testing

--- app/src/main/java/is/handsome/pixelperfectsample/HomeActivity.java
+++ app/src/main/java/is/handsome/pixelperfectsample/HomeActivity.java
@@ -1,12 +1,10 @@
 package is.handsome.pixelperfectsample;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ToggleButton;
 
 import is.handsome.pixelperfectsample.library.ui.PixelPerfectLayout;
 import is.handsome.pixelperfectsample.util.VisualWarnTree;
@@ -20,8 +18,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         pixelPerfectLayout = (PixelPerfectLayout) findViewById(R.id.home_pixel_perfect_layout);
 
@@ -29,40 +25,19 @@ public class HomeActivity extends AppCompatActivity {
             Timber.plant(new Timber.DebugTree());
             Timber.plant(new VisualWarnTree(this));
         }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openPixelPerfectTool(true);
-            }
-        });
     }
 
-    private void openPixelPerfectTool(boolean enabled) {
+    public void changePixelPerfectState(View view) {
+        setPixelPerfectToolVisibility(((ToggleButton) view).isChecked());
+    }
+
+    public void startSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    private void setPixelPerfectToolVisibility(boolean enabled) {
         pixelPerfectLayout.setImageVisible(enabled);
         pixelPerfectLayout.setControlsLayerVisible(enabled);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
